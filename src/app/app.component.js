2126b07f0575944f86eeac8969c11257dc15242d
Duy-Thanh Ngoc Nguyen <Duy-Thanh.Nguyen@avtech.com.au>
2017-09-20 16:31:51 +0700

    Form template

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,28 +5,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var core_1 = require("@angular/core");
-var demo_component_1 = require("./demo.component");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.cities = [
+            { Id: 1, Name: 'HaNoi' },
+            { Id: 2, Name: 'TpHCM' },
+        ];
     }
-    AppComponent.prototype.changeName = function () {
-        this.demoComponent.setName('Changed name');
+    AppComponent.prototype.onSubmit = function (value) {
+        console.log(value);
     };
     return AppComponent;
 }());
-__decorate([
-    core_1.ViewChild(demo_component_1.DemoComponent),
-    __metadata("design:type", demo_component_1.DemoComponent)
-], AppComponent.prototype, "demoComponent", void 0);
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n  <h1>Hello {{name}}</h1>\n  <my-demo></my-demo>\n  <button (click)=\"changeName()\" >Change name</button><br/>\n  <br/>\n  /**\n   * Demo two way binding\n   */ \n  <br/>\n  <br/>\n  <input type=\"text\" [(ngModel)] = \"fname\" />\n  <input type=\"text\" [(ngModel)] = \"lname\" />\n  <br/>\n  Full name : {{fname}} {{lname}}<br/>\n  <br/>\n  /**\n   * customPipes in custom.pipe.ts\n   */\n  <p>2^10 : {{2 | customPipes:10}}</p>\n  ",
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css'],
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
-import { DemoComponent } from './demo.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>Hello {{name}}</h1>
-  <my-demo></my-demo>
-  <button (click)="changeName()" >Change name</button><br/>
-  <br/>
-  /**
-   * Demo two way binding
-   */ 
-  <br/>
-  <br/>
-  <input type="text" [(ngModel)] = "fname" />
-  <input type="text" [(ngModel)] = "lname" />
-  <br/>
-  Full name : {{fname}} {{lname}}<br/>
-  <br/>
-  /**
-   * customPipes in custom.pipe.ts
-   */
-  <p>2^10 : {{2 | customPipes:10}}</p>
-  `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public name = 'Angular';
-  @ViewChild(DemoComponent)
-  private demoComponent: DemoComponent;
-  changeName() {
-    this.demoComponent.setName('Changed name');
+  public cities = [
+    {Id : 1, Name : 'HaNoi'},
+    {Id : 2, Name : 'TpHCM'},
+  ];
+  onSubmit(value: any) {
+    console.log(value);
   }
 }
