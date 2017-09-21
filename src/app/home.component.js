78c42bec4aac6ebc4a5f47eee2dcd5b5c1f2205e
Duy-Thanh Ngoc Nguyen <Duy-Thanh.Nguyen@avtech.com.au>
2017-09-21 11:55:53 +0700

    update button gotoEmployee & goBack

--- src/app/home.component.js
+++ src/app/home.component.js
@@ -5,17 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var HomeComponent = (function () {
-    function HomeComponent() {
+    function HomeComponent(router) {
+        this.router = router;
     }
+    HomeComponent.prototype.goToEmployee = function () {
+        this.router.navigate(['employees']);
+    };
     return HomeComponent;
 }());
 HomeComponent = __decorate([
     core_1.Component({
         selector: 'home-component',
-        template: "<h1>This is home</h1>"
-    })
+        template: "<h1>This is home</h1>\n    <button (click)= \"goToEmployee()\"> Go to Employee</button>"
+    }),
+    __metadata("design:paramtypes", [router_1.Router])
 ], HomeComponent);
 exports.HomeComponent = HomeComponent;
 //# sourceMappingURL=home.component.js.map
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,10 +1,17 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'home-component',
-    template: `<h1>This is home</h1>`
+    template: `<h1>This is home</h1>
+    <button (click)= "goToEmployee()"> Go to Employee</button>`
 })
 
-export class HomeComponent{
-    
+export class HomeComponent {
+
+    constructor(private router: Router) { }
+
+    goToEmployee(): void {
+        this.router.navigate(['employees']);
+    }
 }
